a58f587a79d7d4134fbf6ac646c289862b839783
faisalmirji123 <faisalmirji@gmail.com>
2021-10-29 23:35:43 +0530

    1 file modified

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -4,7 +4,7 @@ import TodoForm from './TodoForm';
    const [tdos,setTodos]=useState([]);
    return (
      <div>
-       <h1> Faisal Mirji Front End Web Developer </h1>
+       <h1> Faisal Front End Web Developer </h1>
        <TodoForm/>
 
        </div>
