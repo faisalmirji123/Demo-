3540cf31f43adea9d279724a9553ee5ea9e8c67b
faisalmirji123 <faisalmirji@gmail.com>
2021-11-27 01:06:02 +0530

    Code Daily

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,16 +1,16 @@
-import React ,{useState} from 'react';
-import TodoForm from './TodoForm';
-import Todo from './Todo'
- function TodoList (){
-   const [todos,setTodos]=useState([]);
-   
-   return (
-     <div>
-       <h1> Faisal Front End Web Developer </h1>
-       <TodoForm/>
-       <Todo/>
-        Hello
-       </div>
-   )
- }
- export default TodoList;
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div>
+      <h1> Faisal Front End Web Developer </h1>
+      <TodoForm />
+      <Todo />
+      Hello Code Daily
+    </div>
+  );
+}
+export default TodoList;
