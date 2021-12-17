139f3296eb2738ae9a5865e83313e8c311bfe670
faisalmirji123 <faisalmirji@gmail.com>
2021-12-17 15:38:46 +0530

    1 file modified

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function Todo() {
   const [todos, setTodos] = useState();
 
-  return <p style={{ textAlign: "center" }}>My name is faisal</p>;
+  return <p style={{ textAlign: "center" }}>Faisal</p>;
 }
 
 export default Todo;
